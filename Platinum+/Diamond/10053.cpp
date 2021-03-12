c08abb01d96d23545213a3a358a3ff27f2fa2654
HyeonS <lkhssa0618@gmail.com>
2021-03-12 03:56:57 +0000

    BOJ 10053: 필승 전략 / Failed

--- Platinum+/Diamond/10053.cpp
+++ Platinum+/Diamond/10053.cpp
@@ -1,12 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int arr[26][26];
+int arr[26][26], before[26][26];
 int piv[26]={0}, bound[26]={0};
 
 bool cmp(string a, string b){
     if(a.size()==b.size()) return a<b;
     return a.size()<b.size();
+}
+
+void floyd(int n){
+    for (int mid=0;mid<n;mid++){
+        for (int start=0;start<n;start++){
+            for (int end=0;end<n;end++){
+                if (arr[start][end] > arr[start][mid] + arr[mid][end]){
+                    arr[start][end] = arr[start][mid] + arr[mid][end];
+                    before[start][end] = before[mid][end];
+                    
+                }
+            }
+        }
+    }
 }
 
 int main(){
@@ -18,9 +32,13 @@ int main(){
     cin >> n;
     vector <vector <string>> v;
 
-    memset(arr, 1234567890, sizeof(arr));
+    for(int i=0; i<n; i++){
+    	for(int j=0; j<n; j++){
+    		arr[i][j]=123456789;
+    	}
+    }
     for(int i=0; i<n; i++) arr[i][i]=0;
-
+    
     int tmp;
     for(int i=0; i<n; i++){
     	v.push_back(vector <string>());
@@ -36,11 +54,12 @@ int main(){
         }
         sort(v[i].begin(), v[i].end(), cmp);
     }
-
+    
     for(int j=0; j<n; j++){
-        while(v[j][piv[j]].size()<=1){
+        while(v[j].size()!=0 && v[j][piv[j]].size()<=1){
         	c=v[j][piv[j]][0];
-            arr[j][(int)c-97]=1;
+        	tmp=c-'a';
+            arr[j][tmp]=1;
             piv[j]++;
         }
     }
@@ -48,32 +67,34 @@ int main(){
     int maxv;
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
-            while(piv[j]<bound[j] && v[j][piv[j]].size()<=i){
+            while(v[j].size()!=0 && piv[j]<bound[j] && v[j][piv[j]].size()<=i){
                 for(int k=0; k<n; k++){
                 	maxv=-1;
                 	if(j!=k){
                 		for(int l=0; l<v[j][piv[j]].size(); l++){
         					c=v[j][piv[j]][l];
-        					if(arr[(int)c-97][k]>maxv) maxv=arr[(int)c-97][k];
+        					tmp=c-'a';
+        					if(arr[tmp][k]>maxv) maxv=arr[tmp][k];
                 		}
-						arr[j][(int)c-97]=min(1234567890, maxv+1);
+						if(arr[j][k]>maxv+1) arr[j][k]=maxv+1;
                 	}
                 }
                 piv[j]++;
             }
+            
+    		memset(before, -1, sizeof(before));
+			for(int l=0; l<n; l++){
+				for(int k=0; k<n; k++){
+					if(arr[l][k]!=123456789) before[l][k]=l;
+				}
+			}
+			floyd(n);
         }
     }
-
-/*
-    for(int i=0; i<n; i++){
-    	for(int j=0; j<v[i].size(); j++) cout << v[i][j] << ' ';
-    	cout << endl;
-    }
-*/
-
+    
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
-            if(arr[i][j]>=1234567890) arr[i][j]=-1;
+            if(arr[i][j]>=123456789) arr[i][j]=-1;
         }
     }
 
