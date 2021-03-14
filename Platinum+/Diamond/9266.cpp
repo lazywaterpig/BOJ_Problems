ed2228aa28827fc442b65d5760b2858e4ffcb7c1
HyeonS <lkhssa0618@gmail.com>
2021-03-14 05:22:30 +0000

    BOJ 9266: Brainstorming

---
+++ Platinum+/Diamond/9266.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int rook_heuristic(int K, int R1, int R2){
+
+}
+
+int king_heuristic(int K, int R1, int R2){
+    
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    int t;
+    cin >> t;
+    while(t--){
+        string k, r1, r2;
+        +
+    }
+
+    return 0;
+}
