8e7b1dda11f4181b266971efa3ca7b2745024a66
HyeonS <lkhssa0618@gmail.com>
2021-03-18 13:52:15 +0000

    BOJ 9663: N-Queen / backtracking

--- solved.ac CLASS/CLASS 4/9663.cpp
+++ solved.ac CLASS/CLASS 4/9663.cpp
@@ -1,9 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int n, cnt;
+int arr[15];
+
+bool possible(int idx){
+    for(int i=idx-1; i>=0; i--){
+        if(arr[i]==arr[idx] || abs(arr[idx]-arr[i])==idx-i) return false;
+    }
+    return true;
+}
+
+void backtrack(int idx){
+    if(idx==n-1){
+        cnt++;
+        return;
+    }
+    for(int i=0; i<n; i++){
+        arr[idx+1]=i;
+        if(possible(idx+1)) backtrack(idx+1);
+    }
+}
+
 int main(){
-
-
+    cin >> n;
+    cnt=0;
+    backtrack(-1);
+    cout << cnt << endl;
+    return 0;
 }
 
 
