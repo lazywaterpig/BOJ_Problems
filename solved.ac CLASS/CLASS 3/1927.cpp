d6341fda1798dbd42528e48c4d14d3a6c7ecfea4
HyeonS <lkhssa0618@gmail.com>
2021-03-18 08:15:37 +0000

    BOJ 1927: 최소 힙 / pq(<int,vector<int>,greater<int>>)

---
+++ solved.ac CLASS/CLASS 3/1927.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+
+int main(){
+    fastio;
+    int n, tmp;
+    cin >> n;
+
+    priority_queue <int, vector<int>, greater<int>> pq;
+    while(n--){
+        cin >> tmp;
+        if(!tmp){
+            if(pq.empty()) cout << 0 << "\n";
+            else{
+                cout << pq.top() << "\n";
+                pq.pop();
+            }
+        }
+        else pq.push(tmp);
+    }
+
+    return 0;
+}
