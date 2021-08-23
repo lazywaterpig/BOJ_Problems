b5aca458b75f14d4bd4c80691381884b8dce36ef
현수 <79351274+lazywaterpig@users.noreply.github.com>
2021-08-23 04:46:03 +0000

    BOJ 10986: 나머지 합 // math, prefix sum

--- Gold-/Gold/10986.cpp
+++ Gold-/Gold/10986.cpp
@@ -1,25 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int arr[100001], cnt[1001];
+long long cnt[1002]={0};
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0);
 
-    int i, j, n, m, ans=0;
+    long long i, j, n, m, v=0, tmp, ans=0;
     cin >> n >> m;
 
-    cin >> arr[0];
-    arr[0]%=m;
-    for(i=1; i<n; i++){
-        cin >> arr[i];
-        arr[i]+=arr[i-1];
-        arr[i]%=m;
+    cnt[0]++;
+    for(i=0; i<n; i++){
+        cin >> tmp;
+        v=(v+tmp)%m;
+        cnt[v]++;
     }
-    for(i=0; i<n; i++) cnt[arr[i]]++;
-    ans+=cnt[0];
-    for(i=1; i<n; i++) ans+=cnt[arr[i]];
+
+    while(m--) ans+=cnt[m]*(cnt[m]-1)/2;
 
     cout << ans;
     return 0;
