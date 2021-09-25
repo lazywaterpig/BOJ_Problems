51f673c5a954b31daff105e51173f86746be48d9
현수 <79351274+lazywaterpig@users.noreply.github.com>
2021-09-25 08:48:11 +0000

    BOJ 2401: 최대 문자열 붙여넣기 // KMP, DP

---
+++ Platinum+/Platinum/2401.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int dp[100002];
+int p[501][10002];
+int sz[501];
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    memset(dp, 0, sizeof(dp));
+    string s; cin >> s;
+    int n; cin >> n;
+
+    string cur[501];
+    for(int i=0; i<n; i++){
+        cin >> cur[i];
+        sz[i]=cur[i].size();
+    }
+
+    memset(p, 0, sizeof(p));
+
+    for(int j=0; j<n; j++){
+        int piv=0;
+        for(int i=1; i<sz[j]; i++){
+            while(piv>0 && cur[i]!=cur[piv]) piv=p[j][piv-1];
+            if(cur[i]==cur[piv]) p[j][i]=++piv;
+        }
+    }
+
+    int piv[501]={0};
+    for(int i=0; i<s.size(); i++){
+        for(int j=0; j<n; j++){
+            while(piv[j]>0 && s[i]!=cur[j][piv[j]]) piv[j]=p[j][piv[j]-1];
+            if(s[i]==cur[j][piv[j]]){
+                if(piv==sz-1){
+                    if(i==0 || i-sz[j]==-1) dp[i]=sz[j];
+                    else dp[i]=max(dp[i], dp[i-sz[j]]+sz[j]);
+                    piv[j]=p[j][piv[j]];
+                }
+                else{
+                    piv[j]++;
+                }
+            }
+        }
+        if(i!=0) dp[i]=max(dp[i-1], dp[i]);
+    }
+
+    cout << dp[s.size()-1];
+    return 0;
+}
